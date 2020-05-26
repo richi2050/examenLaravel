7870606905baf6a84d8e8abef0fd0f0e98ce5755
richi2050 <ricardo.lugo.recillas@gmail.com>
2020-05-25 19:19:51 -0500

    vista por defecto de registro de laravel

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,12 +3,26 @@
  * Created by PhpStorm.
  * User: Ricardo
  * Date: 25/05/20
- * Time: 04:35 PM
+ * Time: 06:35 PM
  */
 
 namespace App\Http\Controllers;
 
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController {
+
+    public function login(Request $request)
+    {
+        $user = User::whereEmail($request->email)->first();
+        if (!is_null($user) && Hash::check($request->password, $user->password)) {
+            $token = $user->createToken('Contactos')->accessToken;
+            return response()->json(['res' => true, 'token' => $token, 'message' => "Bienvenido al sistema"]);
+        } else
+            return response()->json(['res' => false, 'message' => "Cuenta a password incorrectos"]);
+    }
+
 
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-/*use Illuminate\Http\Request;*/
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,11 +13,11 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-/*
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-*/
 
 Route::post('login', 'UserController@login');
 Route::post('register', 'UserController@register');
+
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
